c41bc5fe0037bafb950b2faba03fcb408b5299cc
Siddhi Maroo <siddhi.maroo@intimetec.com>
2026-02-23 16:45:08 +0530

    Add on model creating logic here and add email constarints

--- GoBuddy.Infrastructure/Persistence/AppDbContext.cs
+++ GoBuddy.Infrastructure/Persistence/AppDbContext.cs
@@ -1,16 +1,33 @@
 ﻿using GoBuddy.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace GoBuddy.Infrastructure.Persistence
+namespace GoBuddy.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
+    public DbSet<User> Users { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Vehicle> Vehicles { get; set; }
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .HasIndex(user => user.Email)
+            .IsUnique();
+
+        modelBuilder.Entity<Vehicle>()
+            .HasIndex(vehicle => vehicle.VehicleNo)
+            .IsUnique();
+            
+
+        modelBuilder.Entity<Vehicle>()
+            .HasOne(vehicle => vehicle.Driver)
+            .WithMany(user => user.Vehicles)
+            .HasForeignKey(vehicle => vehicle.FK_user_ID)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
-
-
