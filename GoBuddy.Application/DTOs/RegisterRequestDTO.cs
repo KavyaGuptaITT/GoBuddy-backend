1d3031390c9eee45d9fb1d574fdc7dc6d51a5bab
Siddhi Maroo <siddhi.maroo@intimetec.com>
2026-02-23 16:48:32 +0530

    Modify add name and phone no also

--- GoBuddy.Application/DTOs/RegisterRequestDTO.cs
+++ GoBuddy.Application/DTOs/RegisterRequestDTO.cs
@@ -3,10 +3,10 @@ namespace GoBuddy.BusinessLayer.DTOs
 {
     public class RegisterRequestDTO
     {
+        public string Name { get; set; }
+        public string Phone { get; set; }
         public string Email { get; set; } = null!;
-
         public string Password { get; set; } = null!;
-
         public string Role { get; set; } = null!;
     }
 }
