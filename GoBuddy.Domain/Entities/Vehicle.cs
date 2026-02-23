1a0b906e05679d5cc42ef58bfd5978e33ba55e38
Siddhi Maroo <siddhi.maroo@intimetec.com>
2026-02-23 16:42:48 +0530

    Modify user table add name and phone no. and modify vehicle table make driver if foreign key an dadd constarints

--- GoBuddy.Domain/Entities/Vehicle.cs
+++ GoBuddy.Domain/Entities/Vehicle.cs
@@ -1,11 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using GoBuddy.Domain.Entities;
 namespace GoBuddy.Domain.Entities;
 
 public class Vehicle
 {
-    public Guid VehicleId { get; set; }
-    public Guid DriverId { get; set; }
-    public string VehicleNo { get; set; } = string.Empty;
-    public int TotalSeats { get; set; }
-    public int AvailableSeats { get; set; }
-    public bool IsActive { get; set; } = true;
+    [Key]
+    public int PK_ID { get; private set; }
+    public int FK_user_ID { get; private set; }
+
+    [ForeignKey("FK_user_ID")]
+    public User Driver { get; private set; } = null!;
+
+    public string VehicleNo { get; private set; }
+    public int TotalSeats { get; private set; }
+    public int AvailableSeats { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+
+    public Vehicle() { }
+
+    public Vehicle(int driverId, string vehicleNo, int totalSeats)
+    {
+
+        if (string.IsNullOrWhiteSpace(vehicleNo))
+            throw new ArgumentException("Vehicle number cannot be empty");
+
+        if (totalSeats < 1 || totalSeats > 6)
+            throw new ArgumentException("TotalSeats must be between 1 and 6");
+
+        FK_user_ID = driverId;
+        VehicleNo = vehicleNo;
+        TotalSeats = totalSeats;
+        AvailableSeats = totalSeats;
+        IsActive = true;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
 }
