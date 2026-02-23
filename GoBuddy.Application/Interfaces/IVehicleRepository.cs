d9515ee099a8247dffb3ae0112cadfd932cd3933
Siddhi Maroo <siddhi.maroo@intimetec.com>
2026-02-23 16:49:06 +0530

    Create interfaces for vehicles

---
+++ GoBuddy.Application/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,10 @@
+using GoBuddy.Domain.Entities;
+
+namespace GoBuddy.Application.Interfaces;
+
+public interface IVehicleRepository
+{
+    Task AddAsync(Vehicle vehicle);
+    Task<List<Vehicle>> GetAllAsync();
+    Task<bool> DriverHasActiveVehicleAsync(int driverId);
+}
