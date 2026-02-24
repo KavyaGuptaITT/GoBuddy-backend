5d437beb4f9d2287f8facea8ed8a86ae0032a637
Siddhi Maroo <siddhi.maroo@intimetec.com>
2026-02-24 16:37:25 +0530

    update database and add update location in ridesession table

--- GoBuddy.Infrastructure/Migrations/20260224083151_AddLiveLocationInRideSession.Designer.cs
+++ GoBuddy.Infrastructure/Migrations/20260224083151_AddLiveLocationInRideSession.Designer.cs
@@ -4,6 +4,7 @@ using GoBuddy.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GoBuddy.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260224083151_AddLiveLocationInRideSession")]
+    partial class AddLiveLocationInRideSession
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GoBuddy.Infrastructure/Migrations/20260224083151_AddLiveLocationInRideSession.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoBuddy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLiveLocationInRideSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_RideSessions_CurrentLatitude_CurrentLongitude",
+                table: "RideSessions",
+                columns: new[] { "CurrentLatitude", "CurrentLongitude" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RideSessions_CurrentLatitude_CurrentLongitude",
+                table: "RideSessions");
+        }
+    }
+}
