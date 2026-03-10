1d78c284cb582042a7bcfe3e59cebccf2f468acb
swati-todwal19 <swati.todwal@intimetec.com>
2026-03-10 16:06:52 +0530

    Removed default null value

--- GoBuddy.Application/DTOs/RegisterRequestDTO.cs
+++ GoBuddy.Application/DTOs/RegisterRequestDTO.cs
@@ -1,23 +1,18 @@
 ﻿
+using GoBuddy.Application.DTOs;
 using Microsoft.AspNetCore.Http;
 
 namespace GoBuddy.BusinessLayer.DTOs
 {
     public class RegisterRequestDTO
     {
-        public string? Name { get; set; } 
-        public string? Phone { get; set; } 
-        public string? Email { get; set; } 
-        public string? Password { get; set; } 
-        public string? Role { get; set; } 
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; } 
+        public string Password { get; set; } 
+        public string Role { get; set; } 
         public DateTime Dob { get; set; }
-        public string? VehicleNumber { get; set; }
-        public string? VehicleModel { get; set; }
-        public string? LicenseNumber { get; set; }
-        public int? TotalSeats { get; set; }
-        //public IFormFile? LicenseImg { get; set; }
-       // public IFormFile? VehicleImg { get; set; }
-
+        public VehicleDTO? Vehicle { get; set; }
     }
 }
 
