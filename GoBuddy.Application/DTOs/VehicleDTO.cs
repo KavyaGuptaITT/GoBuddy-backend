48e81283b47a7fccbcb5b7627441c92a5e0a09fa
swati-todwal19 <swati.todwal@intimetec.com>
2026-03-10 16:08:16 +0530

    Removed default null value

--- GoBuddy.Application/DTOs/VehicleDTO.cs
+++ GoBuddy.Application/DTOs/VehicleDTO.cs
@@ -4,10 +4,10 @@ namespace GoBuddy.Application.DTOs;
 
 public class VehicleDTO
 {
-    public string? VehicleNo { get; set; } 
-    public string? VehicleModel { get; set; } 
-    public string? LicenseNo { get; set; } 
+    public string VehicleNo { get; set; }
+    public string VehicleModel { get; set; } 
+    public string LicenseNo { get; set; }
     public int TotalSeats { get; set; }
-    public IFormFile? LicenseImg { get; set; }
-    public IFormFile? VehicleImg { get; set; }
+    //public IFormFile? LicenseImg { get; set; }
+    //public IFormFile? VehicleImg { get; set; }
 }
