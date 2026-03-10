493fff524dff302693f5fd1608591d1dde0889bf
Siddhi Maroo <siddhi.maroo@intimetec.com>
2026-03-10 21:33:37 +0530

    Create enums for status like pending completed canclled

---
+++ GoBuddy.Domain/Enums/RideRequestStatus.cs
@@ -0,0 +1,11 @@
+namespace GoBuddy.Domain.Enums
+{
+    public enum RideRequestStatus
+    {
+        Pending = 0,
+        Accepted = 1,
+        Boarded = 2,
+        Completed = 3,
+        Cancelled = 4
+    }
+}
---
+++ GoBuddy.Domain/Enums/RideSessionStatus.cs
@@ -0,0 +1,9 @@
+namespace GoBuddy.Domain.Enums
+{
+    public enum RideSessionStatus
+    {
+        Active = 0,
+        Cancelled = 1,
+        Completed = 2
+    }
+}
